40c6f89796fb5d96299695caa74b5f76e5227732
Vtx1300 <106390456+Vtx1300@users.noreply.github.com>
2023-04-05 19:09:21 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package Массивы_одномерные_Arrays_metod;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Привет Git");
+        System.out.println("hello world ");
+    }
+}
